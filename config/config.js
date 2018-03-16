9a83f53daeb940853fec48302a548942018c5d8b
yijie.yan <yijie.yan@outlook.com>
2018-03-16 18:18:13 +0800

    delete config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    token: 'haha',
-    appID: 'wx95127461dcb59149',
-    appsecret: 'd93e4937c43f578568a6bca1480fde83',
-    accessKey: 'eUKtRJ51xDTCtFadWZdzFFiPdcg6prKfz4XmTVet',
-    secretKey: '2gtV0NxxL17ccAtv1j4VSjN6XnWizZU-U7H9yT8Y',
-    bucket: 'movies',
+    token: '',
+    appID: '',
+    appsecret: '',
+    accessKey: '',
+    secretKey: '',
+    bucket: '',
     dbUrl: 'mongodb://localhost:27017/video',
-    qiniuUrl: 'http://file.yanyijie.xyz/',
-    weixinUrl: 'yijie123.free.ngrok.cc'
+    qiniuUrl: '',
+    weixinUrl: ''
 };
